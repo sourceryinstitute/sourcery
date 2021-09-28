8af8207477caed22a0aba6f1fdeaba6df310154f
Damian Rouson <damian@sourceryinstitute.org>
2021-09-27 23:06:03 -0700

    feat(separated_values): (sub)module + tests

--- src/formats_m.f90
+++
@@ -1,6 +0,0 @@
-module formats
-  !! Useful strings for formatting `print` and `write` statements
-  implicit none
-
-  character(len=*), parameter :: comma_separated_values = ""(*(G0,:,','))""
-end module
