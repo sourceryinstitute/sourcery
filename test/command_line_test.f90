3078f719878b27f4c1dc83d4c6a53036a876dab6
Damian Rouson <rouson@lbl.gov>
2023-06-03 14:43:27 -0700

    refac(test): delete commented code

--- test/command_line_test.f90
+++ test/command_line_test.f90
@@ -34,7 +34,6 @@ contains
     integer exit_status, command_status
     character(len=132) command_message
 
-    ! command = "fpm run --example get-flag-value -- --input-file ni_weights_iter131072 > /dev/null 2>&1", &
     call execute_command_line( &
       command = "fpm run --example get-flag-value -- --input-file some_file_name", &
       wait = .true., exitstat = exit_status, cmdstat = command_status, cmdmsg = command_message &
