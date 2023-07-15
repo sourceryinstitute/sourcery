3a813680b8be7138abfe3bc99551995506817598
Damian Rouson <rouson@lbl.gov>
2023-07-14 18:18:21 -0700

    fix(sourcery_m):order use/implicit none statements

--- src/sourcery_m.f90
+++ src/sourcery_m.f90
@@ -1,18 +1,20 @@
 module sourcery_m
   !! export all public entities from every other sourcery module
-  implicit none
+  use command_line_m, only : command_line_t
+  use data_partition_m, only : data_partition_t
+  use formats_m, only : formats_t
+  use file_m, only : file_t
+  use string_m, only : string_t
+  use test_result_m, only : test_result_t
+  use test_m, only : test_t
+  use user_defined_collectives_m, only : co_all
 
-  use command_line_m
-  use string_m
-  use file_m
-  use test_result_m
-  use formats_m
-  use oracle_m
-  use object_m
-  use user_defined_collectives_m
-  use data_partition_m
-  use test_m
+  !! legacy modules (likely to be removed in a future release):
+  use object_m, only : object_t
+  use co_object_m, only : co_object_t
+  use oracle_m, only : oracle_t
   use units_m
-  use co_object_m
+
+  implicit none
 
 end module sourcery_m
