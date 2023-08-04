999f40b9b7079ab04f40c7874c3c121e18e89966
Damian Rouson <rouson@lbl.gov>
2023-08-03 19:08:06 -0700

    fix(test): flexible csv real format for nagfor

--- test/formats_test.f90
+++ test/formats_test.f90
@@ -19,7 +19,7 @@ contains
     specimen = "The csv format" 
   end function
 
-  pure function results() result(test_results)
+  function results() result(test_results)
     type(test_result_t), allocatable :: test_results(:)
 
     test_results = [ &
@@ -30,13 +30,31 @@ contains
     ]
   end function
 
-  pure function check_csv_reals() result(test_passes)
+  function check_csv_reals() result(test_passes)
     logical test_passes
     character(len=*), parameter :: expected_output =  "0.00000000,1.00000000,2.00000000"
     character(len=len(expected_output)) captured_output 
 
     write(captured_output, fmt = separated_values(separator=",", mold=[integer::])) [0.,1.,2.]
-    test_passes = expected_output == captured_output
+
+    print *,"fmt = ",separated_values(separator=",", mold=[integer::])
+    print *,captured_output
+
+    block
+      character(len=:), allocatable :: substring 
+
+      substring = adjustl(trim(captured_output))
+      associate(leading_real_zero => index(substring, "0.")==1)
+        substring = adjustl(substring(index(substring, ",")+1:)) ! get remaining string after fierst comma
+        associate(followed_by_real_one => index(substring, "1.")==1)
+          substring = adjustl(substring(index(substring, ",")+1:)) ! get remaining string after fierst comma
+          associate(followed_by_real_two => index(substring, "2.")==1)
+            test_passes = leading_real_zero .and. followed_by_real_one .and.followed_by_real_two
+          end associate
+        end associate
+      end associate
+    end block
+
   end function
 
   pure function check_space_separated_complex() result(test_passes)
