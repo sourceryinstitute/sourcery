718520807ac2c09f390c57544fe6484608823d48
Damian Rouson <rouson@lbl.gov>
2023-08-03 21:24:43 -0700

    fix(test): flexible csv complex format for nagfor

--- test/formats_test.f90
+++ test/formats_test.f90
@@ -32,38 +32,49 @@ contains
 
   function check_csv_reals() result(test_passes)
     logical test_passes
-    character(len=*), parameter :: expected_output =  "0.00000000,1.00000000,2.00000000"
-    character(len=len(expected_output)) captured_output 
+    character(len=*), parameter :: longest_expected_output =  "0.00000000,1.00000000,2.00000000"
+    character(len=len(longest_expected_output)) captured_output 
+    character(len=:), allocatable :: substring 
 
     write(captured_output, fmt = separated_values(separator=",", mold=[integer::])) [0.,1.,2.]
 
-    print *,"fmt = ",separated_values(separator=",", mold=[integer::])
-    print *,captured_output
-
-    block
-      character(len=:), allocatable :: substring 
-
-      substring = adjustl(trim(captured_output))
-      associate(leading_real_zero => index(substring, "0.")==1)
+    substring = adjustl(trim(captured_output))
+    associate(leading_real_zero => index(substring, "0.") == 1)
+      substring = adjustl(substring(index(substring, ",")+1:)) ! get remaining string after fierst comma
+      associate(followed_by_real_one => index(substring, "1.") == 1 )
         substring = adjustl(substring(index(substring, ",")+1:)) ! get remaining string after fierst comma
-        associate(followed_by_real_one => index(substring, "1.")==1)
-          substring = adjustl(substring(index(substring, ",")+1:)) ! get remaining string after fierst comma
-          associate(followed_by_real_two => index(substring, "2.")==1)
-            test_passes = leading_real_zero .and. followed_by_real_one .and.followed_by_real_two
-          end associate
+        associate(followed_by_real_two => index(substring, "2.") == 1 )
+          test_passes = leading_real_zero .and. followed_by_real_one .and.followed_by_real_two
         end associate
       end associate
-    end block
-
+    end associate
   end function
 
-  pure function check_space_separated_complex() result(test_passes)
+  function check_space_separated_complex() result(test_passes)
     logical test_passes 
-    character(len=*), parameter :: expected_output = "(0.00000000,1.00000000) (1.00000000,0.00000000)"
-    character(len=len(expected_output)) captured_output 
+    character(len=*), parameter :: longest_expected_output = "(0.00000000,1.00000000) (1.00000000,0.00000000)"
+    character(len=len(longest_expected_output)) captured_output 
+    complex, parameter :: i = (0.,1.), one = (1.,0.)
+    character(len=:), allocatable :: i_imag_part, one_imag_part, i_string, one_string
 
-    write(captured_output, fmt = separated_values(separator=" ", mold=[complex::])) [(0.,1.),(1.,0.)]
-    test_passes = expected_output == captured_output
+    write(captured_output, fmt = separated_values(separator=" ", mold=[complex::])) i,one
+
+    i_string = adjustl(trim(captured_output(:index(captured_output,")"))))
+    one_string = adjustl(trim(captured_output(len(i_string)+1:)))
+
+    associate( &
+      i_real_part_zero => index(i_string,"(0.") == 1, &
+      one_real_part_one => index(one_string,"(1.") == 1 &
+    )
+      i_imag_part = adjustl(i_string(index(i_string,",")+1:index(i_string,")")-1))
+      one_imag_part = adjustl(one_string(index(one_string,",")+1:index(one_string,")")-1))
+      associate( &
+        i_imag_part_one => index(i_imag_part,"1.") == 1, &
+        one_imag_part_zero => index(one_imag_part,"0.") == 1 &
+      )
+        test_passes = i_real_part_zero .and. i_imag_part_one .and. one_real_part_one .and. one_imag_part_zero
+      end associate
+    end associate
   end function
 
   pure function check_new_line_separated_integers() result(test_passes)
