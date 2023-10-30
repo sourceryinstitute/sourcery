9b444d7f85c8bf73e3bfe87c377ced1851626351
rouson <rouson@lbl.gov>
2023-10-30 02:58:24 +0000

    Deploying to gh-pages from @ sourceryinstitute/sourcery@c932dfdda0cb0c2df56b851d3598d518ce523277 🚀

--- src/sourcery_string_m.f90
+++ src/sourcery_string_m.f90
@@ -14,16 +14,17 @@ module sourcery_string_m
     generic :: string => as_character
     procedure :: is_allocated
     procedure :: get_json_key
-    generic :: operator(/=) => string_t_ne_string_t, string_t_ne_character, character_ne_string_t
-    generic :: operator(==) => string_t_eq_string_t, string_t_eq_character, character_eq_string_t
-    generic :: get_json_value => &
-      get_json_integer_array, get_json_logical, get_json_integer, get_json_string, get_json_real
-    procedure, private :: &
-      get_json_integer_array, get_json_logical, get_json_integer, get_json_string, get_json_real
-    procedure, private :: string_t_ne_string_t, string_t_ne_character
+    generic :: operator(/=)   => string_t_ne_string_t, string_t_ne_character, character_ne_string_t
+    generic :: operator(==)   => string_t_eq_string_t, string_t_eq_character, character_eq_string_t
+    generic :: assignment(= ) => assign_string_t_to_character, assign_character_to_string_t
+    generic :: get_json_value =>     get_json_integer_array, get_json_logical, get_json_integer, get_json_string, get_json_real
+    procedure, private            :: get_json_integer_array, get_json_logical, get_json_integer, get_json_string, get_json_real
+    procedure, private            :: string_t_ne_string_t, string_t_ne_character
+    procedure, private            :: string_t_eq_string_t, string_t_eq_character
+    procedure, private            :: assign_character_to_string_t
     procedure, private, pass(rhs) :: character_ne_string_t
-    procedure, private :: string_t_eq_string_t, string_t_eq_character
     procedure, private, pass(rhs) :: character_eq_string_t
+    procedure, private, pass(rhs) :: assign_string_t_to_character
   end type
 
   interface string_t
@@ -135,6 +136,18 @@ module sourcery_string_m
       character(len=*), intent(in) :: lhs
       logical lhs_ne_rhs
     end function
+
+    pure module subroutine assign_character_to_string_t(lhs, rhs)
+      implicit none
+      class(string_t), intent(inout) :: lhs
+      character(len=*), intent(in) :: rhs
+    end subroutine
+
+    pure module subroutine assign_string_t_to_character(lhs, rhs)
+      implicit none
+      class(string_t), intent(in) :: rhs
+      character(len=:), intent(out), allocatable :: lhs
+    end subroutine
 
   end interface
   
