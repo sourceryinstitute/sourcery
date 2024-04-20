9e10ef9d234ab6067c35430e28df5a1c39f4c34f
Damian Rouson <rouson@lbl.gov>
2024-04-19 17:37:51 -0700

    test(test_description_t): add unit test

--- src/sourcery/sourcery_test_description_m.F90
+++ src/sourcery/sourcery_test_description_m.F90
@@ -25,14 +25,24 @@ module sourcery_test_description_m
   contains
     procedure run
     procedure contains_text
+    generic :: operator(==) => equals
+    procedure, private :: equals
   end type
 
   interface test_description_t
 
-    module function construct(description, test_function) result(test_description)
+    module function construct_from_string_t(description, test_function) result(test_description)
       !! The result is a test_description_t object with the components defined by the dummy arguments
       implicit none
       type(string_t), intent(in) :: description
+      procedure(test_function_i), intent(in), pointer :: test_function
+      type(test_description_t) test_description
+    end function
+
+    module function construct_from_character(description, test_function) result(test_description)
+      !! The result is a test_description_t object with the components defined by the dummy arguments
+      implicit none
+      character(len=*), intent(in) :: description
       procedure(test_function_i), intent(in), pointer :: test_function
       type(test_description_t) test_description
     end function
@@ -54,6 +64,13 @@ module sourcery_test_description_m
       class(test_description_t), intent(in) :: self
       type(string_t), intent(in) :: substring
       logical match
+    end function
+
+    elemental module function equals(lhs, rhs) result(lhs_eq_rhs)
+      !! The result is .true. if the components of the lhs & rhs are equal
+      implicit none
+      class(test_description_t), intent(in) :: lhs, rhs
+      logical lhs_eq_rhs
     end function
 
   end interface
